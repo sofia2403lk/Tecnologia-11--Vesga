1ec1ce04923fb48198d6b4cf1b8c763148bf7948
sofia2403lk <114179400+sofia2403lk@users.noreply.github.com>
2022-09-22 12:29:36 -0500

    Add files via upload

---
+++ ejercicio 3- Vesga.py
@@ -0,0 +1,6 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Usuario/AppData/Local/Programs/Python/Python310/ejercicio 3- vesga.py
+Introduce tu nombre:Karen
+¡HolaKaren!
